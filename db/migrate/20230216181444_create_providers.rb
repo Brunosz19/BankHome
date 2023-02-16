c988d55136309943de83c865a6ea673046f41865
brunosz19 <hesz.bruno05@gmail.com>
2023-02-16 18:24:19 -0500

    HOTFIX: change all data type to string beacuse of the instruction of the technic proof

--- db/migrate/20230216181444_create_providers.rb
+++ db/migrate/20230216181444_create_providers.rb
@@ -2,9 +2,9 @@ class CreateProviders < ActiveRecord::Migration[7.0]
   def change
     create_table :providers do |t|
       t.string :name
-      t.integer :nid
+      t.string :nid
       t.string :contact_name
-      t.integer :phone
+      t.string :phone
 
       t.timestamps
     end
--- db/migrate/20230216181614_create_accounts.rb
+++ db/migrate/20230216181614_create_accounts.rb
@@ -1,7 +1,7 @@
 class CreateAccounts < ActiveRecord::Migration[7.0]
   def change
     create_table :accounts do |t|
-      t.integer :acc
+      t.string :acc
       t.string :name
 
       t.timestamps
