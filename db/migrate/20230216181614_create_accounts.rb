0a5509572eb9042970dee8dc83e02e80abacb738
brunosz19 <hesz.bruno05@gmail.com>
2023-02-17 21:23:39 -0500

    ADD: some validation in the model of Accounts table and in the reference

--- db/migrate/20230216181614_create_accounts.rb
+++ db/migrate/20230216181614_create_accounts.rb
@@ -1,7 +1,7 @@
 class CreateAccounts < ActiveRecord::Migration[7.0]
   def change
     create_table :accounts do |t|
-      t.string :acc
+      t.string :acc, null: true
       t.string :name
 
       t.timestamps
--- db/migrate/20230216182407_add_column_to_account.rb
+++ db/migrate/20230216182407_add_column_to_account.rb
@@ -1,5 +1,5 @@
 class AddColumnToAccount < ActiveRecord::Migration[7.0]
   def change
-    add_reference :accounts, :provider, null: false, foreign_key: true
+    add_reference :accounts, :provider, null: true, foreign_key: true
   end
 end
