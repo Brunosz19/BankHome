b20afcd4c2be0ed613e8e21cb9e5e7e88a183474
brunosz19 <hesz.bruno05@gmail.com>
2023-02-17 21:27:24 -0500

    ADD: new resources for sessions path and for Account index

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,23 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'accounts#index'
+
+  get '/login', to: 'sessions#new'
+  get '/signup', to: 'sessions#signup'
+  devise_scope :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
+
+  Rails.application.routes.draw do
+    get 'sessions/new'
+    post 'sessions/create'
+    delete 'sessions/destroy'
+    get 'sessions/sign_up'
+    post 'sessions/create_user'
+  end
+
+  Rails.application.routes.draw do
+    resources :accounts
+  end
 end
