3c7eb82d2b7fc7b877f91b2b146486d77823751d
brunosz19 <hesz.bruno05@gmail.com>
2023-02-17 22:03:37 -0500

    ADD: resources to make url links to the User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,21 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
+  
   root 'accounts#index'
 
   get '/login', to: 'sessions#new'
   get '/signup', to: 'sessions#signup'
+
   devise_scope :user do
     get '/users/sign_out', to: 'devise/sessions#destroy'
   end
 
-  Rails.application.routes.draw do
-    get 'sessions/new'
-    post 'sessions/create'
-    delete 'sessions/destroy'
-    get 'sessions/sign_up'
-    post 'sessions/create_user'
-  end
+  get 'sessions/new'
+  post 'sessions/create'
+  delete 'sessions/destroy'
+  get 'sessions/sign_up'
+  post 'sessions/create_user'
 
-  Rails.application.routes.draw do
-    resources :accounts
-  end
+  resources :users
+  resources :accounts
 end
