6fc5f26260543beed1991bf3c4aac8d66a46d642
brunosz19 <hesz.bruno05@gmail.com>
2023-02-17 21:24:54 -0500

    ADD: some fake data in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+p "SEED START"
+
+pepe = User.create(:email => "pepe@mail.com", :password => "123123", :password_confirmation => "123123")
+ana = User.create(:email => "ana@mail.com", :password => "123123", :password_confirmation => "123123")
+banks = [ "BCP", "BBVA", "Asteca", "Continental", "Banbif" ]
+
+banks.each do |bank|
+  Account.create(name: bank)
+end
+
+p "SEED FINISHED"
