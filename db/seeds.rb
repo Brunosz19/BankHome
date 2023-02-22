5b1756f6cb74db1b73e834c80cad52a06e1b5df5
brunosz19 <hesz.bruno05@gmail.com>
2023-02-22 01:35:10 -0500

    ADD: some fake information to have dummy data to prove the views

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,13 @@ p "SEED START"
 pepe = User.create(:email => "pepe@mail.com", :password => "123123", :password_confirmation => "123123")
 ana = User.create(:email => "ana@mail.com", :password => "123123", :password_confirmation => "123123")
 banks = [ "BCP", "BBVA", "Asteca", "Continental", "Banbif" ]
+providerOne = Provider.create(name: "Crea", NIT: "901362343-4", contact_name: "Pepe", phone: "987987987")
+providerTwo = Provider.create(name: "Avanza", NIT: "901364564-9", contact_name: "Alisson", phone: "987987123")
+providerThree = Provider.create(name: "International Edu", NIT: "904564343-1", contact_name: "Paco", phone: "987987234")
 
 banks.each do |bank|
   Account.create(name: bank)
 end
+
 
 p "SEED FINISHED"
